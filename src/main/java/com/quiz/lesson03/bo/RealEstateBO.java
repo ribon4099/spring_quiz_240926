6aa886100c217dd50e0206e1e6937512285e76e5
ribon4099 <ribon4099@gmail.com>
2024-10-01 20:01:07 +0900

    241001 spring lesson03 quiz01

---
+++ src/main/java/com/quiz/lesson03/bo/RealEstateBO.java
@@ -0,0 +1,32 @@
+package com.quiz.lesson03.bo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.quiz.lesson03.domain.RealEstate;
+import com.quiz.lesson03.mapper.RealEstateMapper;
+
+@Service
+public class RealEstateBO {
+	
+	@Autowired
+	private RealEstateMapper realEstateMapper;
+
+	// input: id
+	// output: RealEstate
+	public RealEstate getRealEstateById(int id) {
+		return realEstateMapper.selectRealEstateById(id);
+	}
+	
+	// input: rentPrice
+	// output: List<RealEstate>
+	public List<RealEstate> getRealEstateListByRentPrice(int rentPrice111) {
+		return realEstateMapper.selectRealEstateListByRentPrice(rentPrice111);
+	}
+	
+	public List<RealEstate> getRealEstateListByAreaPrice(int area, int price) {
+		return realEstateMapper.selectRealEstateListByAreaPrice(area, price);
+	}
+}
